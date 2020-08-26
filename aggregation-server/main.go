3b602d3803f66ec80f34c31cd074e2e30dc033cc
Joe <joe92v2@gmail.com>
2020-08-26 17:22:41 +0100

    Move flags to first
    Makes sense, no need to initiate things if the flags dont parse correctly

--- aggregation-server/main.go
+++ aggregation-server/main.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
+	port := flag.Int("port", 8000, "The port to run the Aggregator server on")
+	flag.Parse()
+
 	f := &internal.State{Instances: make(map[string]internal.InstanceInfo)}
 	http.HandleFunc("/hello", f.Hello)
 	http.HandleFunc("/bye", f.Bye)
 	http.HandleFunc("/files", f.Files)
-
-	port := flag.Int("port", 8000, "The port to run the Aggregator server on")
-	flag.Parse()
 
 	log.Println("[INFO] Starting aggregator")
 	http.ListenAndServe(":"+strconv.Itoa(*port), nil)
